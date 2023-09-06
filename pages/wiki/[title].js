2494bcf9c28f31c07cd03991635bf51f1f448b07
shoojii012 <shoji_yoshifumi@keio.jp>
2023-09-06 19:52:16 +0900

    コミットできん

--- pages/wiki/[title].js
+++ pages/wiki/[title].js
@@ -20,18 +20,52 @@ const Wiki = () => {
     const router = useRouter();
     const [content, setContent] = useState('');
     const title = router.query.title;
+    var list_of_data;
 
     useEffect(() => {
         if (title) {
             // Wikipedia APIを叩く
             const fetchContent = async () => {
                 try {
-                    const response = await axios.get(`https://ja.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=revisions&titles=${title}&rvprop=content&rvparse`);
-                    const htmlContent = extractHtmlFromResponse(response.data);
-                    setContent(htmlContent);
+                    let continueToken = ''; // 継続トークンを初期化
+
+                    // データのリストを格納するための配列を作成
+                    const listOfData = [];
+                
+                    if(false){//sqlにtitleのやつがあれば
+
+                    }else{
+                        const response = await axios.get(`https://ja.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=revisions&titles=${title}&rvprop=content&rvparse`);
+                        const htmlContent = extractHtmlFromResponse(response.data);
+                        setContent(htmlContent);
+                        
+                        var response_link = await axios.get(`https://ja.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=links&titles=${title}&pllimit=500`);
+                        // console.log(response_link);
+                        var links = response_link.data.query.pages[Object.keys(response_link.data.query.pages)[0]].links;
+                        for (const link of links) {//link名をひたすらlistOfDataに
+                            listOfData.push(link.title);
+                        }
+    
+                        // 継続トークンがなくなるまでデータを取得するためにwhileループを使用
+                        while (response_link.data.hasOwnProperty("continue")) {
+                            continueToken = response_link.data.continue.plcontinue;
+                            response_link = await axios.get(`https://ja.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=links&titles=${title}&plcontinue=${continueToken}&pllimit=500`);
+                            // APIリクエストを送信
+                            // 追加
+                            links = response_link.data.query.pages[Object.keys(response_link.data.query.pages)[0]].links;
+                    
+                            // リンクを反復処理し、listOfDataに
+                            for (const link of links) {//link名をひたすらlistOfDataに
+                                listOfData.push(link.title);
+                            }
+                            
+                        }
+                    }
+
                 } catch (error) {
                     console.error("Error fetching data:", error);
                 }
+
             };
             fetchContent();
         }
